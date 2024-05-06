2d4eb776fe52ec5f9582ffe09e5c02b03b416f25
Alphy <amarslinarulxavier@clarku.edu>
2024-05-06 13:40:39 -0400

    Commit after working on password hash

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -7,7 +7,31 @@ module.exports = gql`
         username: String!
         createdAt: String!
     }
+    
+    type User{
+        id: ID!
+        email: String!
+        token: String!
+        username: String!
+        createdAt: String!
+    }
+
+    input RegisterInput{
+        username: String!
+        password: String!
+        confirmPassword: String!
+        email: String!
+    }
+
     type Query{
         getPosts: [Post]
     }
+
+    type Mutation{
+        register(registerInput: RegisterInput): User!
+    }
 `;
+
+    /* In graphql we are using the Mutation type to register a user.
+    So we are expecting to the receive the inputs of type 'RegisterInput'
+    Once all the received inputs are valid, we then send the output of the type 'User'*/
